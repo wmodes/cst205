d59b1710457ef451fe3653a5a20691392dde70db
Wes Modes <wmodes@gmail.com>
2020-03-25 14:28:55 -0700

    changed photo

--- computer-vision/cv-colormap-ex.py
+++ computer-vision/cv-colormap-ex.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2
 
 # convert image to grayscale
-jeanne_gray = cv2.imread('../images/bit-sm.png', cv2.IMREAD_GRAYSCALE)
-jeanne_remap = cv2.applyColorMap(jeanne_gray, cv2.COLORMAP_WINTER)
+img_obj = cv2.imread('../images/bit-sm.png', cv2.IMREAD_GRAYSCALE)
+img_obj_remap = cv2.applyColorMap(img_obj, cv2.COLORMAP_BONE)
 
 # use highgui to display image
-cv2.imshow("Jeanne in Gray", jeanne_remap)
+cv2.imshow("Jeanne in Gray", img_obj_remap)
 
 # keeps the image displayed
 cv2.waitKey(8000)
